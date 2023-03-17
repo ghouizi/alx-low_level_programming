a35ed041c01b9650eed639dd6b41d657a5810caf
ghouizi <ghouizi1999ilham@gmail.com>
2023-03-16 19:12:26 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(quote) - 1;
+	const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(quote) - 1;
 
-    write(2, quote, len);
+	write(2, quote, len);
 
-    return (1);
+	return (1);
 }
