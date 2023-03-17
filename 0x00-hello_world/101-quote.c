ef8eabf7400a95a1bf8726977a8c911d480a08ff
ghouizi <ghouizi1999ilham@gmail.com>
2023-03-16 19:41:00 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -5,13 +6,9 @@
 *
 * Return: Always 1 (Success)
 */
-
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(quote) - 1;
-
-	write(2, quote, len);
-
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
