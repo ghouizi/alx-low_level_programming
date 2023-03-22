d08ffb2f30046e31843487c394b74ce399bceba4
ghouizi <ghouizi1999ilham@gmail.com>
2023-03-21 17:21:07 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-/**
- * main - function
- * program that prints _putchar
- * Return: The program should return 0
- */
-int main(void)
+#include "main.h"
 
+/**
+ * main - Print puchar,
+ *
+ * Return: Always 0
+ */
+
+int main(void)
 {
-	putchar('_putchar');
-	putchar('\n');
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
