019fa8f27a0e2674b1bf27664986e27ec9b480dc
ghouizi <ghouizi1999ilham@gmail.com>
2023-03-30 18:55:46 -0700

    pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = j = 0;
+
+	// Find the end of the destination string
+	while (dest[i] != '\0');
+	i++;
+
+	// Concatenate the source string to the destination string
+
+	while (src[j] != '\0') 
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	// Add a null terminator to the end of the concatenated string
+	dest[i] = '\0';
+
+	return dest;
+}
