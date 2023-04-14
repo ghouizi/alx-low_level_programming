f8c0c27238a6978488455e8c5296e66dc0bfeec7
ghouizi <ghouizi1999ilham@gmail.com>
2023-04-14 09:21:30 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
